0457e45c2ee62e29697d5edf26e9453cd38579bb
Elmaghwry <mahmoud.elmaghawry@stud.fci-cu.edu.eg>
2018-03-29 22:59:27 +0200

    project initialized

--- app/src/main/java/org/scci_cu/conferencetool/CreateTaskActivity.java
+++ app/src/main/java/org/scci_cu/conferencetool/CreateTaskActivity.java
@@ -1,7 +1,15 @@
 package org.scci_cu.conferencetool;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class CreateTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.date_txt)
+    void select_date() {
+
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+            }
+        };
+
+        new DatePickerDialog(CreateTaskActivity.this,
+                listener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+
     }
 }
--- app/src/main/java/org/scci_cu/conferencetool/LoginActivity.java
+++ app/src/main/java/org/scci_cu/conferencetool/LoginActivity.java
@@ -1,13 +1,55 @@
 package org.scci_cu.conferencetool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
-
+    @BindView(R.id.usr_txt)
+    EditText usr_txt;
+    @BindView(R.id.pass_txt)
+    EditText pass_txt;
+    @BindView(R.id.login_btn)
+    Button login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+    @OnClick(R.id.login_btn)
+    void Login()
+    {
+        if(check(usr_txt) && check(pass_txt))
+        {
+            Intent intent = new Intent(this,CreateTaskActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    boolean check(EditText edit_txt)
+    {
+        String txt = edit_txt.getText().toString().trim();
+        if(txt.isEmpty() || txt == null)
+        {
+            edit_txt.setError("Error");
+            Log.e("Tag","Error");
+            return false;
+        }
+        else
+        {
+            edit_txt.setError(null);
+            return  true;
+        }
     }
 }
