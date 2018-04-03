5537c4a40ed58c7b116f825c3f9afce7f399a4f5
Elmaghwry <mahmoud.elmaghawry@stud.fci-cu.edu.eg>
2018-04-03 20:30:43 +0200

    change in uploud task

--- app/src/main/java/org/scci_cu/conferencetool/CreateTaskActivity.java
+++ app/src/main/java/org/scci_cu/conferencetool/CreateTaskActivity.java
@@ -1,9 +1,13 @@
 package org.scci_cu.conferencetool;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
@@ -20,6 +24,9 @@ public class CreateTaskActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @BindView(R.id.date_txt)
+    TextView date_txt ;
+
     @OnClick(R.id.date_txt)
     void select_date() {
 
@@ -27,7 +34,7 @@ public class CreateTaskActivity extends AppCompatActivity {
         DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-
+                date_txt.setText(year + " - " + (month+1) + " - " + day);
             }
         };
 
@@ -39,4 +46,30 @@ public class CreateTaskActivity extends AppCompatActivity {
                 ).show();
 
     }
+    @BindView(R.id.time_txt)
+    TextView time_txt ;
+    @OnClick (R.id.time_txt)
+    void selecttime(){
+        Calendar calendar = Calendar.getInstance();
+        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int h, int m) {
+                if(h>12) {
+                    h = h - 12;
+                }
+                time_txt.setText(h + ":" + m );
+
+            }
+        };
+
+        new TimePickerDialog(this , listener ,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                false).show();;
+
+
+
+    }
+
+
 }
